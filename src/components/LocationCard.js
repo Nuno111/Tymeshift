d02c8e601bb642edde8eead49a24f463b086be6f
nuno <nuno_c11@hotmail.com>
2021-08-05 12:15:07 +0100

    adjusting paddings to make style as close as possible to design

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -4,13 +4,14 @@ const Card = styled.li`
   background-color: whitesmoke;
   border: 1px solid black;
   width: 300px;
-  margin-top: 4rem;
-  padding: 2rem;
+  margin-top: 2rem;
+  padding: 1rem 1.5rem;
 `;
 
 const CardTitle = styled.p`
   font-size: 1.5rem;
   font-weight: bold;
+  padding-bottom: 0.5rem;
 `;
 
 const LocationCard = ({ name, userCount, createdAt, views }) => {
