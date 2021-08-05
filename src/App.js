a451fc4c983ae030bf15915da58add675c082000
nuno <nuno_c11@hotmail.com>
2021-08-05 10:48:44 +0100

    css reset tested & working

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { Fragment } from "react";
+import { Reset } from "styled-reset";
 
 function App() {
   return (
     <Fragment>
+      <Reset />
       <h1>Hello world</h1>
     </Fragment>
   );
