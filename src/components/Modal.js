b8c5bdca42a8b8c9e9e855c340f215fe152b88a4
nuno <nuno_c11@hotmail.com>
2021-08-05 17:32:42 +0100

    basic modal working

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,13 +1,19 @@
 import styled from "styled-components";
 
-const PageModal = styled.div`
+const ModalOverlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  opacity: 0.4;
   width: 100%;
   height: 100vh;
   background-color: gray;
+  z-index: 2;
+  cursor: pointer;
 `;
-
-const Modal = () => {
-  return <PageModal />;
+// Need to improve accessibility here, add some aria roles or use different html elements
+const Modal = ({ onClick }) => {
+  return <ModalOverlay onClick={onClick} />;
 };
 
 export default Modal;
