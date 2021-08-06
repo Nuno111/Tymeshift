f461730508348816b133eab9df724c5d8e53b7b4
nuno <nuno_c11@hotmail.com>
2021-08-06 18:20:19 +0100

    updatedReadMe

--- src/pages/AllLocations.js
+++ src/pages/AllLocations.js
@@ -35,7 +35,6 @@ const AllLocations = () => {
         const resp = await axios.get(
           "https://6033c4d8843b15001793194e.mockapi.io/api/locations"
         );
-
         setLocationsData(resp.data);
         setLoading(false);
       } catch (err) {
